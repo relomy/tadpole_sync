38eb145524882a69cd7e870d781ff7d3ecc957d3
Adam <relomy@gmail.com>
2019-08-10 08:35:35 -0400

    added logging and a few more functions

--- tadpole.py
+++ tadpole.py
@@ -1,3 +1,5 @@
+import logging
+import logging.config
 from datetime import datetime
 
 import browsercookie
@@ -6,17 +8,23 @@ import requests
 
 from baby_tracker import BabyTracker
 
+# load the logging configuration
+logging.config.fileConfig("logging.ini")
 
-def get_largest_event(event_date):
+logger = logging.getLogger(__name__)
+
+
+def get_largest_event(events, target_date):
     largest_event = {}
     for event in events:
         if event["type"] == "DailyReport":
-            if event["event_date"] == event_date:
-                print(
-                    f"found a daily report. event_date: {event['event_date']}. entries count: {len(event['entries'])}"
-                )
+            if event["event_date"] == target_date:
                 if "entries" in largest_event:
-                    if len(event["entries"]) > len(largest_event["entries"]):
+                    event_size = len(event["entries"])
+                    logger.debug(
+                        f"Found event (size: {event_size}) in DailyReport for {event['event_date']}"
+                    )
+                    if event_size > len(largest_event["entries"]):
                         largest_event = event
                 else:
                     largest_event = event
@@ -30,123 +38,137 @@ def get_utc_date_string(timestamp):
     )
 
 
-url = "https://www.tadpoles.com/remote/v1/events?direction=range&earliest_event_time=1564632000&latest_event_time=1567310400&num_events=300&client=dashboard.com/parents"
+def calculate_duration(start_time, end_time):
+    delta = datetime.fromtimestamp(end_time) - datetime.fromtimestamp(start_time)
+    return int(delta.seconds / 60)
 
 
-cookies = browsercookie.firefox()
+def get_transactions(event):
+    transactions = []
+    if "entries" in event:
+        # loop through each entry
+        for entry in event["entries"]:
+            # skip notes
+            if entry["type"] == "note":
+                continue
 
-r = requests.get(url, cookies=cookies)
+            # set actor (used for BabyTracker note) based on entry fields, if there is one
+            actor = "..no one?"
+            if "parent" in entry and entry["parent"] == True:
+                actor = "Parent"
+            else:
+                if "actor" in entry and entry["actor"]:
+                    actor = entry["actor"]
+                elif "prepared_actor" in entry and entry["prepared_actor"]:
+                    actor = entry["prepared_actor"]
 
-json = r.json()
+            if "start_time" in entry:
+                start_time = get_utc_date_string(entry["start_time"])
+            else:
+                raise Exception("No start_time found in entry")
 
-events = {}
-
-if "events" in json:
-    events = json["events"]
-else:
-    raise ("There are no events in the response.")
-
-
-my_date = "2019-08-09"
-largest_event = get_largest_event(my_date)
-
-
-print("biggest entries: ", len(largest_event["entries"]))
-
-transactions = []
-
-if "entries" in largest_event:
-    # loop through each entry
-    for entry in largest_event["entries"]:
-        actor = None
-        start_time = None
-
-        if "start_time" in entry:
-            start_time = get_utc_date_string(entry["start_time"])
-
-        if "parent" in entry and entry["parent"] == True:
-            actor = "Parent"
-        else:
-            if "actor" in entry and entry["actor"]:
-                actor = entry["actor"]
-
-            if "prepared_actor" in entry and entry["prepared_actor"]:
-                actor = entry["prepared_actor"]
-
-        if entry["type"] == "bathroom":
-            print(
-                "bathroom found: actor: {} start_time: {} classification: {}".format(
-                    actor, entry["start_time"], entry["classification"]
-                )
-            )
-            transactions.append(
-                {
-                    "type": "diaper",
-                    "actor": actor,
-                    "start_time": start_time,
-                    "classification": entry["classification"],
-                }
-            )
-
-        elif entry["type"] == "food":
-
-            quantity = entry["quantity"]
-            amount_offered = None
-            contents = None
-
-            if "amount_offered" in entry:
-                amount_offered = entry["amount_offered"]
-
-            if "contents" in entry:
-                contents = entry["contents"]
-
-            print(
-                "bottle found: actor: {} start_time: {} contents: {}".format(
-                    actor, entry["start_time"], contents
-                )
-            )
-            transactions.append(
-                {
-                    "type": "meal",
-                    "actor": actor,
-                    "start_time": start_time,
-                    "quantity": quantity,
-                    "amount_offered": amount_offered,
-                    "contents": contents,
-                }
-            )
-        elif entry["type"] == "nap":
-            if "start_time" in entry and "end_time" in entry:
-                stop_time = get_utc_date_string(entry["end_time"])
-
-                delta = datetime.fromtimestamp(entry["end_time"]) - datetime.fromtimestamp(
-                    entry["start_time"]
-                )
-
-                duration = int(delta.seconds / 60)
-
-                print(
-                    "completed nap found: actor: {} start_time: {} stop_time: {} duration: {}".format(
-                        actor, entry["start_time"], entry["end_time"], duration
-                    )
-                )
+            if entry["type"] == "bathroom":
+                logger.info(f"Found a diaper @ {start_time}")
+                # determine type of diaper to send to BabyTracker
+                if "Wet" in entry["classification"]:
+                    diaper_type = "wet"
+                elif "BM" in entry["classification"]:
+                    diaper_type = "dirty"
+                elif "Dry" in entry["classification"]:
+                    diaper_type = "dry"
+                else:
+                    raise Exception(f"Unsupported diaper type: {entry['classification']}")
 
                 transactions.append(
                     {
-                        "type": "nap",
+                        "type": "diaper",
                         "actor": actor,
                         "start_time": start_time,
-                        "end_time": datetime.fromtimestamp(entry["end_time"]).strftime(
-                            "%Y-%m-%d %H:%M:%S"
-                        ),
-                        "duration": duration,
+                        "diaper_type": diaper_type,
                     }
                 )
 
-tracker = BabyTracker()
-tracker.create_transactions(transactions)
+            elif entry["type"] == "food":
+                logger.info(f"Found a meal @ {start_time}")
+                quantity = entry["quantity"]
+                amount_offered = None
+                contents = None
 
-# TODO
-# create baby tracker events
-# check if baby tracker events exist
-#
+                if "amount_offered" in entry:
+                    amount_offered = entry["amount_offered"]
+
+                if "contents" in entry:
+                    contents = entry["contents"]
+
+                transactions.append(
+                    {
+                        "type": "meal",
+                        "actor": actor,
+                        "start_time": start_time,
+                        "quantity": quantity,
+                        "amount_offered": amount_offered,
+                        "contents": contents,
+                    }
+                )
+            elif entry["type"] == "nap":
+                logger.info(f"Found a nap @ {start_time}")
+                # completed naps only - must have an end_time
+                if "end_time" in entry:
+                    end_time = get_utc_date_string(entry["end_time"])
+
+                    # calculate duration
+                    duration = calculate_duration(entry["start_time"], entry["end_time"])
+
+                    transactions.append(
+                        {
+                            "type": "nap",
+                            "actor": actor,
+                            "start_time": start_time,
+                            "end_time": end_time,
+                            "duration": duration,
+                        }
+                    )
+    else:
+        raise Exception("No entries found in largest_event")
+
+    return transactions
+
+
+def main():
+    logger.info("Getting events from https://www.tadpoles.com")
+    logger.debug("Getting cookies from Firefox")
+    # TODO - authenticate properly with requests?
+    cookies = browsercookie.firefox()
+    url = "https://www.tadpoles.com/remote/v1/events?direction=range&earliest_event_time=1564632000&latest_event_time=1567310400&num_events=300&client=dashboard.com/parents"
+
+    logger.debug(f"Perform GET request. URL: {url}")
+    r = requests.get(url, cookies=cookies)
+    json = r.json()
+
+    events = {}
+    if "events" in json:
+        events = json["events"]
+    else:
+        raise Exception("There are no events in the response.")
+
+    my_date = "2019-08-09"
+    logger.info(f"Getting largest event for {my_date}")
+    event = get_largest_event(events, my_date)
+
+    if event == None:
+        raise Exception("Event is empty.")
+
+    logger.info(f"Getting transactions for event (date: {event['event_date']})")
+    transactions = get_transactions(event)
+
+    # TODO
+    # create baby tracker events
+    # check if baby tracker events exist
+    logger.info("Creating transactions in BabyTracker")
+    tracker = BabyTracker()
+    tracker.create_transactions(transactions)
+
+
+if __name__ == "__main__":
+    main()
+
